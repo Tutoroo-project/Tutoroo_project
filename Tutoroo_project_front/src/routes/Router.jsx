a8721d11dcedaa33e9fce0a7dcb62af43206686b
BNMAMA <ksuyeon56@naver.com>
2026-01-15 17:46:19 +0900

    <feat>(global): subscribe,payment 구현 중

--- Tutoroo_project_front/src/routes/Router.jsx
+++ Tutoroo_project_front/src/routes/Router.jsx
@@ -3,15 +3,15 @@ import DashboardPage from "../pages/dashboards/DashboardPage";
 import LoginPage from "../pages/auths/LoginPage";
 import PasswordVerifyPage from "../pages/mypage/PasswordVerifyPage";
 import ChangeInfoPage from "../pages/mypage/ChangeInfoPage";
-import Sidebar from "../pages/mypage/Sidebar";
 import ChangePasswordPage from "../pages/mypage/ChangePasswordPage";
 import WithdrawalPage from "../pages/mypage/WithdrawalPage";
 import RankingPage from "../pages/ranking/RankingPage";
-
 import TutorSelectionPage from "../pages/tutor/TutorSelectionPage";
 import StudyPage from "../pages/studys/StudyPage";
 import LevelTestPage from "../pages/leveltests/LevelTestPage";
 import LevelTestResultPage from "../pages/leveltests/LevelTestResultPage";
+import Payments from "../pages/payments/Payments";
+import PaymentPage from "../pages/payments/PaymentPage";
 
 function Router() {
   return (
@@ -19,14 +19,12 @@ function Router() {
       <Route path="/" element={<DashboardPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/mypage/verify" element={<PasswordVerifyPage />} />
-      {/* <Route path="sidebar" element={<Sidebar />} /> */}
       <Route path="mypage/changeinfo" element={<ChangeInfoPage />} />
       <Route path="mypage/changepassword" element={<ChangePasswordPage />} />
-      <Route path="mypage/withdrawal" element={<WithdrawalPage />} />
-      
       <Route path="mypage/withdrawl" element={<WithdrawalPage />} />
       <Route path="/ranking" element={<RankingPage />} />
-
+      <Route path="/subscribe" element={<Payments />} />
+      <Route path="/subscribe/payment" element={<PaymentPage />} />
       <Route path="/level-test" element={<LevelTestPage />} />
       <Route path="/level-test/result" element={<LevelTestResultPage />} />
       <Route path="/tutor" element={<TutorSelectionPage />} />
